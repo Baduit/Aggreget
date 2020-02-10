687c06fbd04c42769565f967d8328fa4653f9b34
Baduit <simon.bertho@epitech.eu>
2020-02-10 23:05:30 +0100

    Add Aggregate concept everywhere and foreach function

--- src/aggreget/aggreget.hpp
+++ src/aggreget/aggreget.hpp
@@ -5,16 +5,22 @@
 namespace aggreget
 {
 
-template <typename T>
+template <Aggregate T>
 constexpr auto to_tuple(const T& t)
 {
 	return details::to_tuple_impl(t);
 }
 
-template <std::size_t Index, typename T>
+template <std::size_t Index, Aggregate T>
 constexpr auto get(const T& t)
 {
 	return std::get<Index>(to_tuple(t));
+}
+
+template <Aggregate T, typename Cb>
+auto foreach(const T& t, Cb&& cb)
+{
+	return std::apply([&](auto&&... args) {(cb(args), ...);}, to_tuple(t));
 }
 
 }
--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,10 @@
 #include <cassert>
 #include <string>
 #include <iostream>
+#include <concepts>
+#include <string>
+#include <vector>
+#include <string_view>
 
 #include "aggreget/aggreget.hpp"
 
@@ -19,7 +23,41 @@ void basic_test()
 	assert(aggreget::get<2>(s) == 42.42);
 }
 
+bool dummy_not_error_proof_start_with_because_still_not_implemented_and_that_make_me_sad(std::string_view str, std::string_view substr)
+{
+	if (substr.size() > str.size())
+		return false;
+
+	for (std::size_t i = 0; i < substr.size(); ++i)
+		if (str[i] != substr[i])
+			return false;
+	
+	return true;
+}
+
+void foreach_test()
+{
+	std::vector<std::string> results;
+	auto cb =
+		[&](const auto& arg)
+		{
+			using T = std::decay_t<decltype(arg)>;
+			if constexpr (std::same_as<T, std::string>)
+				results.push_back(arg);
+			else
+				results.push_back(std::to_string(arg));
+		};
+
+	Struct s = {"Yolol", 42, 42.42 };
+	aggreget::foreach(s, cb);
+
+	assert(results[0] == "Yolol");
+	assert(results[1] == "42");
+	assert(dummy_not_error_proof_start_with_because_still_not_implemented_and_that_make_me_sad(results[2], "42.42"));
+}
+
 int main()
 {
 	basic_test();
+	foreach_test();
 }
