1041f9f9e82042544b22b581601d890a0620e72b
Baduit <simon.bertho@epitech.eu>
2020-02-12 21:21:54 +0100

    First version of the conversion function

--- src/aggreget/aggreget.hpp
+++ src/aggreget/aggreget.hpp
@@ -23,6 +23,29 @@ constexpr auto foreach(const T& t, Cb&& cb)
 	return std::apply([&](auto&&... args) {(cb(args), ...);}, to_tuple(t));
 }
 
+// Still early stage and expirimental
+template <Aggregate Output, Aggregate Input>
+auto convert(Input&& i)
+{
+	// Later add a static assert so that Output and Input have the same number of argument
+	return std::apply(
+		[&](auto&&... args)
+		{
+			return Output { std::move(args)... };
+		},
+		to_tuple(std::move(i)));
+}
 
+template <Aggregate Output, Aggregate Input>
+auto convert(const Input& i)
+{
+	// Later add a static assert so that Output and Input have the same number of argument
+	return std::apply(
+		[&](const auto&... args)
+		{
+			return Output { args... };
+		},
+		to_tuple(i));
+}
 
 }
